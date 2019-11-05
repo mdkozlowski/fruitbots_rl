96547aaa2ba5ec5d580e04d6e24e380ca4b4b661
Marcin <marcin.kozlowski122@gmail.com>
2019-11-05 22:55:32 +0200

    flesh out gym env, add rewards & state vecs

--- engine.py
+++ engine.py
@@ -7,7 +7,8 @@ FRUIT_TYPES = {
 }
 
 DEFAULT_CONFIG = {
-	"fruits_max": 0.3,
+	"fruits_max_prop": 0.5,
+	"max_turns": 200,
 }
 
 ACTIONS = {
@@ -44,19 +45,23 @@ class Map:
 		self.size = size
 		self.fruit = []
 		self.bots = []
+		self.max_fruit = 0
 
 		self.new_map()
 
 	def new_map(self):
-		max_fruit = int(DEFAULT_CONFIG["fruits_max"] * self.size ** 2)
+		self.max_fruit = int(DEFAULT_CONFIG["fruits_max_prop"] * self.size ** 2)
+
 		fruit_pos = []
-		for idx in range(max_fruit):
-			candidate = tuple(np.random.randint(0, self.size, size=2))
-			if candidate not in fruit_pos:
-				fruit_pos.append(candidate)
-		for pos in fruit_pos:
-			fruit = Fruit(np.array([pos[0], pos[1]]), np.random.choice(3))
-			self.fruit.append(fruit)
+		self.fruit_assignments = np.repeat(np.array([0, 1, 2]), self.max_fruit / 3)
+		for idx in range(self.max_fruit):
+			while True:
+				candidate = tuple(np.random.randint(0, self.size, size=2))
+				if candidate not in fruit_pos:
+					fruit_pos.append(candidate)
+					fruit = Fruit(np.array([candidate[0], candidate[1]]), self.fruit_assignments[idx])
+					self.fruit.append(fruit)
+					break
 
 		while True:
 			candidate = tuple(np.random.randint(0, self.size, size=2))
@@ -68,9 +73,27 @@ class Map:
 class Engine:
 	def __init__(self):
 		self.turn = 0
+		self.done = False
+		self.won = False
 		self.map = Map(5)
 
+	def get_state_vec(self):
+		map_vec = np.zeros((4, self.map.size, self.map.size))
+		state = np.zeros(4)  # fruit, turn
+
+		bot = self.map.bots[0]
+		for fruit in self.map.fruit:
+			map_vec[fruit.ftype, fruit.position[0], fruit.position[1]] = 1
+		map_vec[3, bot.position[0], bot.position[1]] = 1
+
+		state[0:3] = self.map.bots[0].owned_fruit / np.bincount(self.map.fruit_assignments)
+		state[-1] = self.turn / DEFAULT_CONFIG["max_turns"]
+
+		return map_vec, state
+
 	def step(self, action):
+		if self.turn > DEFAULT_CONFIG["max_turns"]:
+			self.done = True
 		self.turn += 1
 
 		bot_pos = self.map.bots[0].position
@@ -81,7 +104,7 @@ class Engine:
 			pos_delta[1] += 1
 		elif action == ACTIONS["LEFT"]:
 			pos_delta[0] -= 1
-		elif action == ACTIONS["UP"]:
+		elif action == ACTIONS["RIGHT"]:
 			pos_delta[0] += 1
 
 		new_pos = bot_pos + pos_delta
@@ -92,6 +115,10 @@ class Engine:
 		else:
 			self.map.bots[0].position = new_pos
 			for fruit in self.map.fruit:
-				if fruit.position == new_pos:
+				if np.array_equal(fruit.position, new_pos):
 					self.map.bots[0].owned_fruit[fruit.ftype] += 1
 					self.map.fruit.remove(fruit)
+
+		if len(self.map.fruit) == 0:
+			self.done = True
+			self.won = True
--- fruitbots_env.py
+++ fruitbots_env.py
@@ -1,23 +1,48 @@
 import gym
 from gym import spaces
-from engine import Engine
+from engine import Engine, DEFAULT_CONFIG
 
 
 class FruitbotsEnvSP(gym.Env):
 	def __init__(self):
 		self.turn = 0
 		self.engine = Engine()
+		self.action_space = spaces.Discrete(4)
+		self.observation_space = spaces.Dict({
+			"map_vec": spaces.Box(0, 1, (4, self.engine.map.size, self.engine.map.size)),
+			"state": spaces.Box(0, 1, shape=(4,))
+		})
+		print(self.observation_space.sample())
 
 	def step(self, action):
 		assert type(action) is int
 
 		self.engine.step(action)
+		current_state = self.engine.get_state_vec()
+
+		reward = 0
+		if self.engine.done and self.engine.won:
+			reward = 1.0
+
+		if not self.engine.done:
+			reward += -(self.engine.turn / DEFAULT_CONFIG["max_turns"])
+
+		return current_state, reward, self.engine.done, {}
 
 	def reset(self):
 		self.turn = 0
 		self.engine = Engine()
 
+		return self.engine.get_state_vec()
+
 	def render(self, mode='human'):
+		print(f"Fruit eaten: {self.engine.map.max_fruit - len(self.engine.map.fruit):3d}. Turn {self.engine.turn:3d}")
 		pass
 
 	pass
+
+
+if __name__ == '__main__':
+	env = FruitbotsEnvSP()
+	env.step(0)
+	print(env.engine.get_state_vec())
